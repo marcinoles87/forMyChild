dd418061931e5fa34b4210cc2b10ff1ff0f3b6d0
unknown <marcinoles87@gmail.com>
2022-11-14 12:47:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
+import app from "../src/app.css" ;
+
+
+
 
 function App() {
+
+  const randomNumber = Math.floor(Math.random()*10);
+  const randomNumber2 = Math.floor(Math.random()*10);
+
+  const [number , setNumber] = React.useState(0);
+
+ const  result = randomNumber*randomNumber2;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> poczatkowa liczba to {number} </p>
+      <p> pierwsya liczba to {randomNumber}</p>
+      <p> druga liczba liczba to {randomNumber2}</p>
+
+      <h1> Jaki jest wynik działania :  {randomNumber} razy {randomNumber2}</h1>
+      <h2> Wynik mnożenia to : {result}</h2>
+      
+
+      <button onClick={ () => setNumber(number+1)}> Click me to add 1</button>
+     
     </div>
   );
 }
