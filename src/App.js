1d9c21c8e4caba08efd134e83e004020730fddc8
unknown <marcinoles87@gmail.com>
2022-11-15 09:32:23 +0100

    new function input

--- src/App.js
+++ src/App.js
@@ -10,18 +10,38 @@ function App() {
   const randomNumber2 = Math.floor(Math.random()*10);
 
   const [number , setNumber] = React.useState(0);
+  const [value , setValue] = React.useState('');
 
  const  result = randomNumber*randomNumber2;
+
+
+ const handleOnChange = (e) => setValue(e.target.value);
+
+ const handleOnClickButton = () => {
+  console.log("dzialam");
+ 
+}
 
   return (
     <div className="App">
       <p> poczatkowa liczba to {number} </p>
-      <p> pierwsya liczba to {randomNumber}</p>
+      <p> pierwsza liczba to {randomNumber}</p>
       <p> druga liczba liczba to {randomNumber2}</p>
 
       <h1> Jaki jest wynik działania :  {randomNumber} razy {randomNumber2}</h1>
-      <h2> Wynik mnożenia to : {result}</h2>
-      
+      <h2> Podaj wynik mnożenia : 
+
+        <input type="number" className="inputValue" onChange={handleOnChange} value={value}></input>
+        
+        </h2>
+
+
+        <button onClick={handleOnClickButton}>Zatwierdzam</button>
+
+
+
+
+    
 
       <button onClick={ () => setNumber(number+1)}> Click me to add 1</button>
      
